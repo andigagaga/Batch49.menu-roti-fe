784ea55b67eb614357ffcd6b0391292170673045
andigagaga <andigagaga1@gmail.com>
2023-10-03 11:38:00 +0700

    lanjut

---
+++ src/app/Story.tsx
@@ -0,0 +1,56 @@
+export default function Story() {
+    return (
+        <>
+        <div className="flex">
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1695750733098-2e3617fa43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+                alt=""
+                className="w-12 mb-3 ml-8 mt-8 border-2 border-red-500 h-12 bg-blue-500 rounded-full flex-shrink-0"
+              />
+              <h5 className="text-white pl-5">Guswan_di</h5>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1695750733098-2e3617fa43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+                alt=""
+                className="w-12 mb-3 ml-8 mt-8 border-2 border-red-500 h-12 bg-blue-500 rounded-full flex-shrink-0"
+              />
+              <h5 className="text-white pl-5">Guswan_di</h5>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1695750733098-2e3617fa43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+                alt=""
+                className="w-12 mb-3 ml-8 mt-8 border-2 border-red-500 h-12 bg-blue-500 rounded-full flex-shrink-0"
+              />
+              <h5 className="text-white pl-5">Guswan_di</h5>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1695750733098-2e3617fa43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+                alt=""
+                className="w-12 mb-3 ml-8 mt-8 border-2 border-red-500 h-12 bg-blue-500 rounded-full flex-shrink-0"
+              />
+              <h5 className="text-white pl-5">Guswan_di</h5>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1695750733098-2e3617fa43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+                alt=""
+                className="w-12 mb-3 ml-8 mt-8 border-2 border-red-500 h-12 bg-blue-500 rounded-full flex-shrink-0"
+              />
+              <h5 className="text-white pl-5">Guswan_di</h5>
+            </div>
+            <div>
+              <img
+                src="https://images.unsplash.com/photo-1695750733098-2e3617fa43df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80"
+                alt=""
+                className="w-12 mb-3 ml-8 mt-8 border-2 border-red-500 h-12 bg-blue-500 rounded-full flex-shrink-0"
+              />
+              <h5 className="text-white pl-5">Guswan_di</h5>
+            </div>
+          </div>
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,9 @@
-// import Image from 'next/image';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHouse,
-  faSearch,
-  faCompass,
-  faComment,
-  faBell,
-  faEdit,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import Menu from "./Menu";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-gradient-to-r from-blue-500 to-purple-500 h-screen flex">
-        <div className="w-1/4 bg-white p-8 ">
-          <div className="flex mb-12 font-extrabold">
-            <FontAwesomeIcon
-              icon={faInstagram}
-              className="text-red-500 mr-2 w-6 h-6"
-            />
-            <h1>Instagram</h1>
-          </div>
-          <div className="my-8 flex flex-col ">
-            <div className="flex  mb-4">
-              <FontAwesomeIcon icon={faHouse} className="mr-2 w-4 h-4" />
-              <h1>Houme</h1>
-            </div>
-            <div className="flex mb-4">
-              <FontAwesomeIcon icon={faSearch} className="mr-2 w-4 h-4" />
-              <h1>Search</h1>
-            </div>
-            <div className="flex mb-4">
-              <FontAwesomeIcon icon={faCompass} className="mr-2 w-4 h-4" />
-              <h1>Explore</h1>
-            </div>
-            <div className="flex mb-4">
-              <FontAwesomeIcon icon={faComment} className="mr-2 w-4 h-4" />
-              <h1>Message</h1>
-            </div>
-            <div className="flex mb-4">
-              <FontAwesomeIcon icon={faBell} className="mr-2 w-4 h-4" />
-              <h1>Notification</h1>
-            </div>
-            <div className="flex mb-4">
-              <FontAwesomeIcon icon={faEdit} className="mr-2 w-4 h-4" />
-              <h1>Create</h1>
-            </div>
-            <div className="flex mb-4">
-              <FontAwesomeIcon icon={faUser} className="mr-2 w-4 h-4" />
-              <h1>Profile</h1>
-            </div>
-          </div>
-        </div>
-        <div className="w-3/4">{/* Isi konten kanan di sini */}</div>
-      </div>
+     <Menu />
     </>
   );
 }
